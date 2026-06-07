5ec2de82d5f67322899ea5921ae32f454d43f5b1
Prateek <pratk2k7@gmail.com>
2026-06-07 09:56:46 +0530

    fix cors for netlify

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(cors({
     origin:[
-        "https://expensetrackerpd.netlify.app/"
+        "https://expensetrackerp.netlify.app/"
     ],
       methods: ["GET", "POST", "PUT", "DELETE"],
     credentials:true
